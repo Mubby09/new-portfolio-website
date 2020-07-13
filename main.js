58021c794b6d4f0814b8ae0abfe8f6d94314a755
Mubarak <akinsolamubarak1996@gmail.com>
2020-07-13 15:46:18 +0100

    Resume update

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 const navbarButton = document.querySelector(".navbar-toggle");
 const navbar = document.querySelector(".navbar");
-const link = document.querySelector(".link");
-
-link.addEventListener("click", (e) => {
-  e.preventDefault();
-});
 
 navbarButton.addEventListener("click", () => {
   navbar.classList.toggle("show");
