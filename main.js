a237c6a157f483d4481564835cd8bf252654b0f1
Mubarak <akinsolamubarak1996@gmail.com>
2020-07-14 14:47:03 +0100

    links update

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const navbarButton = document.querySelector(".navbar-toggle");
 const navbar = document.querySelector(".navbar");
+const link1 = document.querySelector(".list-item1");
+const link2 = document.querySelector(".list-item2");
+const link3 = document.querySelector(".list-item3");
 
 navbarButton.addEventListener("click", () => {
   navbar.classList.toggle("show");
+});
+
+link1.addEventListener("click", () => {
+  navbar.classList.remove("show");
+});
+link2.addEventListener("click", () => {
+  navbar.classList.remove("show");
+});
+link3.addEventListener("click", () => {
+  navbar.classList.remove("show");
 });
